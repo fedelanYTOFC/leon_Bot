3b32b2a10d1265cad3b0b10c37a3c40c7ef56bf9
fedelanYTOFC <fedelan150@gmail.com>
2025-05-25 22:17:47 -0300

    Update rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn, text }) => {
 let user = global.db.data.users[m.sender]
 
 user.registered = false
-return conn.reply(m.chat, `*Se elimino el registro en la base de datos 🌙*`, m, rcanal)
+return conn.reply(m.chat, `*🚮 Tu registro fue eliminado.*`, fkontak)
 
 }
 handler.help = ['unreg']
 handler.tags = ['rg']
-handler.command = ['unreg']
+handler.command = /^unreg(ister)?$/i
 handler.register = true
 export default handler
