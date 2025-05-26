eb4afd192b081a1a65df288ed2f0273ef469934a
fedelanYTOFC <fedelan150@gmail.com>
2025-05-25 22:45:28 -0300

    Update Info•creador.js

--- plugins/Info•creador.js
+++ plugins/Info•creador.js
@@ -1,1 +1,47 @@
-// Pronto
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+   await m.react('☕');
+
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+    let name = await conn.getName(who);
+    let edtr = `@${m.sender.split`@`[0]}`;
+    let username = conn.getName(m.sender);
+
+    // VCARD
+    let list = [{
+        displayName: "fedelanYT",
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN: Cristian Escobar
+\nitem1.TEL;waid=5491156178758:5491156178758\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET: cristianescobar.vx@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/dev.criss_vx\nitem3.X-ABLabel:Internet\nitem4.ADR:;; Perú 🇵🇪;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
+    }];
+
+    await conn.sendMessage(m.chat, {
+        contacts: {
+            displayName: `${list.length} Contacto`,
+            contacts: list
+        },
+        contextInfo: {
+            externalAdReply: {
+                showAdAttribution: true,
+                title: 'Hello, I am the official creator of Shadow Ultra.',
+                body: dev,
+                thumbnailUrl: 'https://files.catbox.moe/8an9fs.jpg',
+                sourceUrl: 'https://wa.me/51927238856?text=Hola+quiero+adquirir+bot',
+                mediaType: 1,
+                renderLargerThumbnail: true
+            }
+        }
+    }, {
+        quoted: m
+    });
+
+    let txt = `👋 *Hola \`${username}\` este es*\n*el contacto de mi desarrollador*`;
+
+    await conn.sendMessage(m.chat, { text: txt });
+};
+
+handler.help = ['owner', 'creador'];
+handler.tags = ['info'];
+handler.command = /^(owner|creator|creador|dueño)$/i;
+
+export default handler;
